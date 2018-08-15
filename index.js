600736f681f726f715ed479561069395a72b19b7
YanC Lin <yonalin1@gmail.com>
2018-08-15 21:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const driver = 'bob';
+
 console.log('this code was called')
- console.log('driver')
-  console.log('driver','is the driver variable name')
+console.log(driver)
+console.log(driver,'is the driver variable name')
